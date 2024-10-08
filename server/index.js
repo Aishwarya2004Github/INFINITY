459af274b3b17b1fafc3fd131295fb0b9d01b165
Aishwarya Rana <160381307+Aishwarya2004Github@users.noreply.github.com>
2024-10-08 11:07:26 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -39,8 +39,11 @@ app.post('/send-audio', upload.single('audio'), (req, res) => {
     res.status(400).send('Error uploading file.');
   }
 });
-mongoose.connect(MONGO_URL)
-
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("DB Connection Successful");
   })
